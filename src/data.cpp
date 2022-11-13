e7025d2e183da5349b949c6da89f6c28d5b05c8a
root <vinitg2@illinois.edu>
2022-11-13 20:27:00 +0000

    airport + airline + routes

--- src/data.cpp
+++ src/data.cpp
@@ -1,6 +1,7 @@
 #include "data.h"
 
-Data::Data(string filename) {
+Airport_data::Airport_data(string filename) {
+    vector<line> store;
     ifstream wordsFile(filename);
     if (wordsFile.is_open()) {
         line l;
@@ -30,42 +31,138 @@ Data::Data(string filename) {
             getline(wordsFile, l.database, ',');
             getline(wordsFile, l.Type, ',');
             getline(wordsFile, l.Source, '\n');
-            store.push_back(l);
+            airport x = conv_line(l);
+            airport_data.push_back(x);
             line l;
         }
     } else {
         cout << "Error opening file" << endl;
     }
-    makestdata(store);
 }
 
+airport Airport_data::conv_line(line& st) {
+    airport tmp;
+    tmp.id = stoi(st.id);
 
-void Data::makestdata(vector<line> st) {
-    // cout << st[116].Source << endl;
-    // cout << i << endl;
-    //     if (i == 6981) cout << "XXXXX =  " << st[i].id << " = " << st[i].Timezone  << endl;
-    for (size_t i = 0; i < st.size(); i++) {
-        stdata tmp;
-        tmp.id = stoi(st[i].id);
+    tmp.Name = st.Name.substr(1, st.Name.size() - 2);
+    tmp.City = st.City.substr(1, st.City.size() - 2);
+    tmp.Country = st.Country.substr(1, st.Country.size() - 2);
+    tmp.IATA = st.IATA.substr(1, st.IATA.size() - 2);
+    tmp.ICAO = st.ICAO.substr(1, st.ICAO.size() - 2);
 
-        tmp.Name = st[i].Name.substr(1, st[i].Name.size() - 2);
-        tmp.City = st[i].City.substr(1, st[i].City.size() - 2);
-        tmp.Country = st[i].Country.substr(1, st[i].Country.size() - 2);
-        tmp.IATA = st[i].IATA.substr(1, st[i].IATA.size() - 2);
-        tmp.ICAO = st[i].ICAO.substr(1, st[i].ICAO.size() - 2);
+    tmp.Latitude = stof(st.Latitude);
+    tmp.Longitude = stof(st.Longitude);
+    tmp.Altitude = stoi(st.Altitude);
+    if (st.Timezone != "\\N"){
+        tmp.Timezone = stof(st.Timezone);
+    } else tmp.Timezone = -100;
 
-        tmp.Latitude = stof(st[i].Latitude);
-        tmp.Longitude = stof(st[i].Longitude);
-        tmp.Altitude = stoi(st[i].Altitude);
-        if (st[i].Timezone != "\\N"){
-            tmp.Timezone = stof(st[i].Timezone);
-        } else tmp.Timezone = -100;
+    tmp.DST = st.DST.substr(1, st.DST.size() - 2);
+    tmp.database = st.database.substr(1, st.database.size() - 2);
+    tmp.Type = st.Type.substr(1, st.Type.size() - 2);
+    tmp.Source = st.Source.substr(1, st.Source.size() - 2);
+    return tmp;
+}
 
-        tmp.DST = st[i].DST.substr(1, st[i].DST.size() - 2);
-        tmp.database = st[i].database.substr(1, st[i].database.size() - 2);
-        tmp.Type = st[i].Type.substr(1, st[i].Type.size() - 2);
-        tmp.Source = st[i].Source.substr(1, st[i].Source.size() - 2);
+Airline_data::Airline_data(string filename) {
+    ifstream wordsFile(filename);
+    if (wordsFile.is_open()) {
+        airline l;
+        string id;
+        while (getline(wordsFile, id, ',')) {
+            l.id = stoi(id);
+            getline(wordsFile, l.Name, ',');
+            if (l.Name[l.Name.size() - 1] != '"') {
+                string t;
+                getline(wordsFile, t, ',');
+                l.Name += ',' + t;
+            }
 
-        mainstore.push_back(tmp);
+            getline(wordsFile, l.Alias, ',');
+            getline(wordsFile, l.IATA, ',');
+            getline(wordsFile, l.ICAO, ',');
+            getline(wordsFile, l.Callsign, ',');
+            getline(wordsFile, l.Country, ',');
+            getline(wordsFile, l.Active, '\n');
+            airline x = conv_line(l);
+            airline_data.push_back(x);
+            airline l;
+        }
+    } else {
+        cout << "Error opening file" << endl;
     }
 }
+
+airline Airline_data::conv_line(airline& l) {
+    airline x;
+    x.id = l.id;
+    x.Name = l.Name.substr(1, l.Name.size() - 2);
+    x.Alias = l.Alias.substr(1, l.Alias.size() - 2);
+    if (l.IATA == "") {
+        x.IATA = l.IATA;
+    } else x.IATA = l.IATA.substr(1, l.IATA.size() - 2);
+    if (x.ICAO == "") {
+        x.ICAO = l.ICAO;
+    } else x.ICAO = l.ICAO.substr(1, l.ICAO.size() - 2);
+    if (l.Callsign == "") {
+        x.Callsign = l.Callsign;
+    } else x.Callsign = l.Callsign.substr(1, l.Callsign.size() - 2);
+    if (l.Country == "") {
+        x.Country = l.Country;
+    } else x.Country = l.Country.substr(1, l.Country.size() - 2);
+    x.Active = l.Active.substr(1, l.Active.size() - 2);
+
+    return x;
+}
+
+Route_data::Route_data(string filename) {
+    ifstream wordsFile(filename);
+    if (wordsFile.is_open()) {
+        route l;
+        while (getline(wordsFile, l.airline_code, ',')) {
+            string id;
+            getline(wordsFile, id, ',');
+            if (id == "\\N") {
+                l.id = -100;
+            } else l.id = stoi(id);
+            
+            getline(wordsFile, l.source_code, ',');
+            string s_id;
+            getline(wordsFile, s_id, ',');
+            if (s_id == "\\N") {
+                l.source_id = -100;
+            } else l.source_id = stoi(s_id);
+            getline(wordsFile, l.dest_code, ',');
+            string d_id;
+            getline(wordsFile, d_id, ',');
+            if (d_id == "\\N") {
+                l.dest_id = -100;
+            } else l.dest_id = stoi(d_id);
+            getline(wordsFile, l.Codeshare, ',');
+            string s;
+            getline(wordsFile, s, ',');
+            l.Stops = stoi(s);
+            getline(wordsFile, l.Equipment, '\n');
+            route_data.push_back(l);
+            route l;
+        }
+    } else {
+        cout << "Error opening file" << endl;
+    }
+}
+
+// route Route_data::conv_line(route& l) {
+//     route x;
+//     x.airline_code = l.airline_code.substr(1, l.airline_code.size() - 2);
+//     x.id = l.id;
+//     x.source_code = l.source_code.substr(1, l.source_code.size() - 2);
+//     x.source_id = l.source_id;
+//     x.dest_code = l.dest_code.substr(1, l.dest_code.size() - 2);
+//     x.dest_id = l.dest_id;
+//     if (l.Codeshare == "") {
+//         x.Codeshare = l.Codeshare;
+//     } else x.Codeshare = l.Codeshare.substr(1, l.Codeshare.size() - 2);
+//     x.Stops = l.Stops;
+//     x.Equipment = l.Equipment.substr(1, l.Equipment.size() - 2);
+//     return x;
+// }
--- src/data.h
+++ src/data.h
@@ -7,24 +7,7 @@
 using std::ifstream;
 using namespace std;
 
-struct line {
-    string id;
-    string Name;
-    string City;
-    string Country;
-    string IATA;
-    string ICAO;
-    string Latitude;
-    string Longitude;
-    string Altitude;
-    string Timezone;
-    string DST;
-    string database; 
-    string Type;
-    string Source;
-};
-
-struct stdata {
+struct airport {
     int id;
     string Name;
     string City;
@@ -41,13 +24,72 @@ struct stdata {
     string Source;
 };
 
-class Data {
+class Airport_data {
+    
     public:
-        Data(string filename);
-        vector<stdata> getdata() {return mainstore;}
+        Airport_data(string filename);
+        vector<airport> getdata() {return airport_data;};
     private:
-        vector<line> store;
-        vector<stdata> mainstore;
-        void makestdata(vector<line> st);
-        
+        struct line {
+            string id;
+            string Name;
+            string City;
+            string Country;
+            string IATA;
+            string ICAO;
+            string Latitude;
+            string Longitude;
+            string Altitude;
+            string Timezone;
+            string DST;
+            string database; 
+            string Type;
+            string Source;
+        };
+        vector<airport> airport_data;
+        airport conv_line(line& st);
+        // void makedata(vector<line> st);
+
+};
+
+struct airline {
+    int id;
+    string Name;
+    string Alias;
+    string IATA;
+    string ICAO;
+    string Callsign;
+    string Country;
+    string Active;
+};
+
+class Airline_data {
+    public:
+        Airline_data(string filename);
+        vector<airline> getdata() {return airline_data;};
+    private:
+        vector<airline> airline_data;
+        airline conv_line(airline& l);
+
+};
+
+struct route {
+    string airline_code;
+    int id;
+    string source_code;
+    int source_id;
+    string dest_code;
+    int dest_id;
+    string Codeshare;
+    int Stops;
+    string Equipment;
+};
+
+class Route_data {
+    public:
+        Route_data(string filename);
+        vector<route> getdata() {return route_data;};
+    private:
+        vector<route> route_data;
+        // route conv_line(route& l);
 };
