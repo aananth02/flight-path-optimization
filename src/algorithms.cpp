45021c67730e324e31e654a578585788e7451c43
akasharunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2022-12-02 00:29:25 +0000

    Added documentation.

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -3,55 +3,61 @@
 #include <map>
 #include <algorithm>
 
-void Algorithms::Floyd_Warshall(const Graph& g) {
+/// @brief Travellers like to keep their travelling time to a minimum. In most parts of the world, 
+/// travel time correlates with the distance between the passenger's Point of Departure and Point of Arrival
+/// We're using Floyd-Warshall's algorithm to find the minimum distance.
+/// @param inputGraph Our graph of airports and routes, implemented using an adjacency matrix.
+void Algorithms::Floyd_Warshall(const Graph& inputGraph) {
 
-    vector<float> tmp(g.getVertices().size(), 25000);
-    vector<vector<float>> d(g.getVertices().size(), tmp);
-    vector<Vertex> tmp2(g.getVertices().size(), "");
-    p = vector<vector<Vertex>>(g.getVertices().size(), tmp2);
-    for (Vertex v : g.getVertices()) {
-        d[g.getVertexIdx(v)][g.getVertexIdx(v)] = 0;
+    vector<float> tmp(inputGraph.getVertices().size(), 25000);
+    vector<vector<float>> d(inputGraph.getVertices().size(), tmp);
+    vector<Vertex> tmp2(inputGraph.getVertices().size(), "");
+    paths = vector<vector<Vertex>>(inputGraph.getVertices().size(), tmp2);
+    for (Vertex v : inputGraph.getVertices()) {
+        d[inputGraph.getVertexIdx(v)][inputGraph.getVertexIdx(v)] = 0;
     }
-    for (auto i : g.getEdges()) {
+    for (auto i : inputGraph.getEdges()) {
         if (i.weight > 0)
-            d[g.getVertexIdx(i.src)][g.getVertexIdx(i.dest)] = i.weight;
+            d[inputGraph.getVertexIdx(i.src)][inputGraph.getVertexIdx(i.dest)] = i.weight;
     }
-    for (Vertex v : g.getVertices()) {
-        for (Vertex u : g.getVertices()) {
-            if (d[g.getVertexIdx(v)][g.getVertexIdx(u)] != 25000)
-                p[g.getVertexIdx(v)][g.getVertexIdx(u)] = v;
+    for (Vertex v : inputGraph.getVertices()) {
+        for (Vertex u : inputGraph.getVertices()) {
+            if (d[inputGraph.getVertexIdx(v)][inputGraph.getVertexIdx(u)] != 25000)
+                paths[inputGraph.getVertexIdx(v)][inputGraph.getVertexIdx(u)] = v;
         }
     }
-    for (Vertex w : g.getVertices()) {
-        for (Vertex u : g.getVertices()) {
-            for (Vertex v : g.getVertices()) {
-                int u_idx = g.getVertexIdx(u);
-                int v_idx = g.getVertexIdx(v);
-                int w_idx = g.getVertexIdx(w);
+    for (Vertex w : inputGraph.getVertices()) {
+        for (Vertex u : inputGraph.getVertices()) {
+            for (Vertex v : inputGraph.getVertices()) {
+                int u_idx = inputGraph.getVertexIdx(u);
+                int v_idx = inputGraph.getVertexIdx(v);
+                int w_idx = inputGraph.getVertexIdx(w);
                 if (d[u_idx][v_idx] > d[u_idx][w_idx] + d[w_idx][v_idx]) {
                     d[u_idx][v_idx] = d[u_idx][w_idx] + d[w_idx][v_idx];
-                    p[u_idx][v_idx] = p[w_idx][v_idx];
+                    paths[u_idx][v_idx] = paths[w_idx][v_idx];
                 }
             }
         }
     }
 }
 
-// @brief find betweenes centrality in graph, meaning the most dense airport, 
-// i.e. to a traveller, we're finding the transit hubs!
-/// @param g adjacency graph
-/// @param vert vector obtained from Algorithms::Path
+/// @brief Travelling through airports that are prominent hubs in a region is extremely fun! Bustling energy, duty-free shopping, and more!
+/// We're using the Betweenes Centrality algorithm to find the transit hub, 
+/// i.e. the most densely/well-connected airport between a Passenger's Point of Departure and Point of Arrival!
+/// i.e. to a traveller, we're finding the transit hubs!
+/// @param inputGraph adjacency graph
+/// @param vertices vector obtained from Algorithms::Path
 /// @return return vertex
-std::string between_centrality(const Graph &g, vector<std::string> vert)
+std::string Algorithms::BetweennessCentrality(const Graph &inputGraph, vector<std::string> vertices)
 {
     int max_count = 0;
     std::string central;
-    for (auto vertex1 : vert)
+    for (auto vertex1 : vertices)
     {
         int count = 0;
-        for (auto vertex2 : vert)
+        for (auto vertex2 : vertices)
         {
-            if (g.edgeExists(vertex1, vertex2))
+            if (inputGraph.edgeExists(vertex1, vertex2))
             {
                 count++;
             }
@@ -70,13 +76,13 @@ std::string between_centrality(const Graph &g, vector<std::string> vert)
 /// @param source 
 /// @param destination 
 /// @return 
-vector<string> Algorithms::Path(const Graph& g, Vertex source, Vertex destination) {
-    if (p[g.getVertexIdx(source)][g.getVertexIdx(destination)] == "")
+vector<string> Algorithms::Path(const Graph& inputGraph, Vertex source, Vertex destination) {
+    if (paths[inputGraph.getVertexIdx(source)][inputGraph.getVertexIdx(destination)] == "")
         return {};
     vector<string> path = { destination };
 
     while (source != destination) {
-        destination = p[g.getVertexIdx(source)][g.getVertexIdx(destination)];
+        destination = paths[inputGraph.getVertexIdx(source)][inputGraph.getVertexIdx(destination)];
         path.push_back(destination);
     }
     return path;
@@ -86,7 +92,7 @@ vector<string> Algorithms::Path(const Graph& g, Vertex source, Vertex destinatio
 // In the context of our Airports and Routes Graph, a Minimum Spanning Tree is basically a backpacker's gudie to the world:
 // The cheapest way to cover the world, the cheapest possible next destination from anywhere they are right now!
 /// @param inputGraph : Our original graph of airport and routes data, consistsing of all possible edges, etc.
-/// @return : Minimum Spanning Tree of
+/// @return : Minimum Spanning Tree of out airports and routes graph.
 Graph Algorithms::Kruskal(Graph inputGraph) {
     //  Disjoint Set setup   
     DisjointSets forest;
