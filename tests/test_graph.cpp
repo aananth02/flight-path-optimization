0d1133ca8ae8c3f7e34a41663586144b9ffd38d5
root <vinitg2@illinois.edu>
2022-12-13 04:41:16 +0000

    modified test cases

--- tests/test_graph.cpp
+++ tests/test_graph.cpp
@@ -35,8 +35,13 @@ TEST_CASE("Edge Weights and Labels Small", "[graph]")
     vector<float> weights = {distance(-6.081689834590001, 145.391998291, -5.826789855957031, 144.29600524902344),
                              distance(-6.081689834590001, 145.391998291, -6.569803, 146.725977),
                              distance(-6.081689834590001, 145.391998291, -5.20707988739, 145.789001465),
-                             distance(-6.081689834590001, 145.391998291, -9.443380355834961, 147.22000122070312)};
-    vector<string> labels = {"CG", "CG", "CG", "CG"};
+                             distance(-6.081689834590001, 145.391998291, -9.443380355834961, 147.22000122070312),
+                             distance(-5.826789855957031, 144.29600524902344, -6.569803, 146.725977),
+                             distance(-6.569803, 146.725977, -5.20707988739, 145.789001465),
+                             distance(-5.20707988739, 145.789001465, -3.58383011818, 143.669006348),
+                             distance(-9.443380355834961, 147.22000122070312, -5.826789855957031, 144.29600524902344),
+                             distance(-9.443380355834961, 147.22000122070312, -6.569803, 146.725977)};
+    vector<string> labels = {"CG", "CG", "CG", "CG", "CG", "CG", "CG", "CG", "CG"};
     for (size_t i = 0; i < edges.size(); i++)
     {
         REQUIRE(edges[i].weight == weights[i]);
@@ -51,43 +56,43 @@ TEST_CASE("Edge Weights and Labels Small", "[graph]")
 // Tests on Larger Dataset
 // Note: These tests take a long time to compile and run
 
-Route_data test_routes2("../data/routes_cleaned.csv");
-Graph test_graph_large(test_airport1, test_routes2);
+// Route_data test_routes2("../data/routes_cleaned.csv");
+// Graph test_graph_large(test_airport1, test_routes2);
 
-TEST_CASE("Graph Creation Large", "[graph]")
-{
-    REQUIRE(test_graph_large.getVertices().size() == 1631);
-    REQUIRE(test_graph_large.getEdges().size() == 10806);
-    REQUIRE(test_graph_large.isDirected());
-}
+// TEST_CASE("Graph Creation Large", "[graph]")
+// {
+//     REQUIRE(test_graph_large.getVertices().size() == 1631);
+//     REQUIRE(test_graph_large.getEdges().size() == 10806);
+//     REQUIRE(test_graph_large.isDirected());
+// }
 
-TEST_CASE("Node Adjacency Large", "[graph]")
-{
-    vector<string> Rankin_Adj_source{"Baker Lake Airport", "Chesterfield Inlet Airport", "Churchill Airport",
-                                     "Repulse Bay Airport", "Coral Harbour Airport", "Yellowknife Airport"};
-    vector<Vertex> adj = test_graph_large.getAdjacentDir("Rankin Inlet Airport", 1);
-    for (size_t i = 0; i < adj.size(); i++)
-    {
-        REQUIRE(adj[i] == Rankin_Adj_source[i]);
-    }
-}
+// TEST_CASE("Node Adjacency Large", "[graph]")
+// {
+//     vector<string> Rankin_Adj_source{"Baker Lake Airport", "Chesterfield Inlet Airport", "Churchill Airport",
+//                                      "Repulse Bay Airport", "Coral Harbour Airport", "Yellowknife Airport"};
+//     vector<Vertex> adj = test_graph_large.getAdjacentDir("Rankin Inlet Airport", 1);
+//     for (size_t i = 0; i < adj.size(); i++)
+//     {
+//         REQUIRE(adj[i] == Rankin_Adj_source[i]);
+//     }
+// }
 
-TEST_CASE("Edge Weights and Labels Large", "[graph]")
-{
+// TEST_CASE("Edge Weights and Labels Large", "[graph]")
+// {
 
-    vector<float> weights = {distance(68.78170013427734, 32.75080108642578, 69.68329620361328, 18.918899536132812),
-                             distance(-43.48939895629883, 172.53199768066406, -41.298301696777344, 173.2209930419922)};
-    vector<string> labels = {"5N", "NZ"};
+//     vector<float> weights = {distance(68.78170013427734, 32.75080108642578, 69.68329620361328, 18.918899536132812),
+//                              distance(-43.48939895629883, 172.53199768066406, -41.298301696777344, 173.2209930419922)};
+//     vector<string> labels = {"5N", "NZ"};
 
-    Edge MMK_TOS = test_graph_large.getEdge("Murmansk Airport", "Tromsø Airport,");
-    Edge CHC_NSN = test_graph_large.getEdge("Christchurch International Airport", "Nelson Airport");
-    Edge NO_EXIST = test_graph_large.getEdge("Busselton Regional Airport", "Christchurch International Airport");
+//     Edge MMK_TOS = test_graph_large.getEdge("Murmansk Airport", "Tromsø Airport,");
+//     Edge CHC_NSN = test_graph_large.getEdge("Christchurch International Airport", "Nelson Airport");
+//     Edge NO_EXIST = test_graph_large.getEdge("Busselton Regional Airport", "Christchurch International Airport");
 
-    REQUIRE(MMK_TOS.weight == weights[0]);
-    REQUIRE(CHC_NSN.weight == weights[1]);
-    REQUIRE(int(NO_EXIST.weight) == 0);
+//     REQUIRE(MMK_TOS.weight == weights[0]);
+//     REQUIRE(CHC_NSN.weight == weights[1]);
+//     REQUIRE(int(NO_EXIST.weight) == 0);
 
-    REQUIRE(MMK_TOS.edge_label == labels[0]);
-    REQUIRE(CHC_NSN.edge_label == labels[1]);
-    REQUIRE(NO_EXIST.edge_label == "");
-}
+//     REQUIRE(MMK_TOS.edge_label == labels[0]);
+//     REQUIRE(CHC_NSN.edge_label == labels[1]);
+//     REQUIRE(NO_EXIST.edge_label == "");
+// }
